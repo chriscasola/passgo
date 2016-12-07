ddd22748fed8146265e62ece04dc9e7471a2cc63
Chris Casola <chriscasola@gmail.com>
2016-12-06 20:53:26 -0500

    Output hashed passwords in base64 encoding and expect passwords to be verified in base64 encoding.

--- passgo.go
+++ passgo.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"crypto/rand"
 	"crypto/sha256"
+	"encoding/base64"
 	"fmt"
 	"golang.org/x/crypto/pbkdf2"
 )
@@ -17,14 +18,14 @@ const maxPasswordSize = 256
 // and other metadata about the hash.
 type HashedPassword struct {
 	IterationCount int
-	Salt           []byte
-	Hash           []byte
+	Salt           string
+	Hash           string
 	Alg            string
 }
 
 // Hash generates a HashedPassword from the given plain-text
 // password.
-func Hash(password []byte) (*HashedPassword, error) {
+func Hash(password string) (*HashedPassword, error) {
 	// verify the password is not too long to reasonably calculate
 	// a hash for (avoid DOS)
 	if len(password) > maxPasswordSize {
@@ -39,21 +40,35 @@ func Hash(password []byte) (*HashedPassword, error) {
 	}
 
 	// do the hashing
-	derivedKey := pbkdf2.Key(password, salt, iterationCount, keySize, sha256.New)
+	derivedKey := pbkdf2.Key([]byte(password), salt, iterationCount, keySize, sha256.New)
 
-	return &HashedPassword{IterationCount: iterationCount, Salt: salt, Hash: derivedKey, Alg: "sha256"}, nil
+	// base64 encode
+	encodedKey := base64.StdEncoding.EncodeToString(derivedKey)
+	encodedSalt := base64.StdEncoding.EncodeToString(salt)
+
+	return &HashedPassword{IterationCount: iterationCount, Salt: encodedSalt, Hash: encodedKey, Alg: "sha256"}, nil
 }
 
 // Verify check if the given plain-text password hashes to the
 // same value as HashedPassword
-func Verify(password []byte, hashed *HashedPassword) bool {
+func Verify(password string, hashed *HashedPassword) bool {
 	// verify the password is not too long to reasonably calculate
 	// a hash for (avoid DOS)
 	if len(password) > maxPasswordSize {
 		return false
 	}
 
-	derivedKey := pbkdf2.Key(password, hashed.Salt, hashed.IterationCount, len(hashed.Hash), sha256.New)
+	// base64 decode
+	key, err := base64.StdEncoding.DecodeString(hashed.Hash)
+	if err != nil {
+		return false
+	}
+	salt, err := base64.StdEncoding.DecodeString(hashed.Salt)
+	if err != nil {
+		return false
+	}
 
-	return bytes.Equal(derivedKey, hashed.Hash)
+	derivedKey := pbkdf2.Key([]byte(password), salt, hashed.IterationCount, len(key), sha256.New)
+
+	return bytes.Equal(derivedKey, key)
 }
--- passsgo_test.go
+++ passsgo_test.go
@@ -1,36 +1,34 @@
 package passgo
 
 import (
-	"math/rand"
+	"strconv"
 	"testing"
 )
 
 func TestPassGo(t *testing.T) {
-	result, err := Hash([]byte("my secret password"))
+	result, err := Hash("my secret password")
 
 	if err != nil {
 		t.Errorf("Error creating hash: %v", err)
 	}
 
-	if !Verify([]byte("my secret password"), result) {
+	if !Verify("my secret password", result) {
 		t.Error("Password did not verify")
 	}
 
-	if Verify([]byte("WRONG password"), result) {
+	if Verify("WRONG password", result) {
 		t.Error("Password should not verify")
 	}
 }
 
 func TestLimits(t *testing.T) {
-	password := make([]byte, 257)
+	password := ""
 
-	_, err := rand.Read(password)
-
-	if err != nil {
-		t.Error("Error generating test password")
+	for i := 0; i < 257; i++ {
+		password += strconv.Itoa(i)
 	}
 
-	_, err = Hash(password)
+	_, err := Hash(password)
 
 	if err == nil {
 		t.Error("Should prevent long password")
